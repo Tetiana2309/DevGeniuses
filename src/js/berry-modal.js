4620642bcac90cb34b25c94ff1491c5f32d84c54
Tetiana2309 <126595700+Tetiana2309@users.noreply.github.com>
2023-05-24 13:42:30 +0200

    щл

--- src/js/berry-modal.js
+++ src/js/berry-modal.js
@@ -23,5 +23,5 @@ closeBtns.forEach(btn => {
 form.addEventListener('submit', e => {
   e.preventDefault();
   setTimeout(openSuccessModal, 300);
-  //   setTimeout(closeSuccessModal, 2400);
+  setTimeout(closeSuccessModal, 2400);
 });
